a9435c3c4c06de83842375e6569573218645ec00
Ciara <ciara.f.mellows@gmail.com>
2023-10-15 17:22:55 +0100

    some more movement stuff + crouching

--- !proto/proto_character.gd
+++ !proto/proto_character.gd
@@ -7,7 +7,12 @@ extends CharacterBody3D
 @export var ACCEL : float = 60.0
 const STOP_SPEED : float = 0.5
 
-@export var JUMP_VELOCITY : float = 4.5
+@export var HEIGHT : float = 1.8
+@export var HEIGHT_CROUCH : float = 1.0
+var current_height : float
+var crouching : bool = false
+
+@export var JUMP_VELOCITY : float = 3.5
 
 @export var SENSITIVITY : float = 1.5
 const MOUSE_SCALE : float = PI/3600
@@ -17,8 +22,12 @@ var speed : float
 var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	current_height = HEIGHT
+	$stand_check.position.y = HEIGHT_CROUCH
+	$stand_check.target_position.y = HEIGHT - HEIGHT_CROUCH
 	speed = SPEED
+	set_height(HEIGHT)
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
@@ -28,15 +37,20 @@ func _physics_process(delta):
 	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	elif velocity:
+		var flat_velocity = velocity - Vector3.UP*velocity.y
+		$leading_edge.global_position = position + flat_velocity.normalized()*0.2 + Vector3.UP
 
 	if Input.is_action_just_pressed("J") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	crouching = Input.is_action_pressed("C") or $stand_check.is_colliding()
+	update_height(delta)
 
 	var input_dir = Input.get_vector("M_L", "M_R", "M_F", "M_B")
 	var wish_3 = (transform.basis * Vector3(input_dir.x, 0.0, input_dir.y)).normalized()
 	
-	friction(delta)
+	friction(delta, input_dir)
 	
 	if input_dir:
 		if is_on_floor():
@@ -47,16 +61,37 @@ func _physics_process(delta):
 
 	move_and_slide()
 
+func set_height(new:float):
+	$visuals.mesh.height = new
+	$visuals.position.y = new*0.5
+	$main_camera.position.y = new - 0.35
+	$cap.shape.height = new - 0.1
+	$cap.position.y = new*0.5 + 0.05
+	
+	current_height = new
+	
+
+func update_height(delta:float):
+	var target_height = HEIGHT_CROUCH if crouching else HEIGHT
+	if current_height != (target_height):
+		var new_height = lerp(current_height, target_height, 10.0*delta)
+		if abs(new_height-target_height) <= 0.005:
+			new_height = target_height
+		if !is_on_floor():
+			position.y -= new_height - current_height
+		set_height(new_height)
+
 func mouse_look(by:Vector2):
 	by *= MOUSE_SCALE * SENSITIVITY
 	rotate_y(by.x)
 	main_camera.rotate_x(by.y)
 
-func friction(delta:float):
+func friction(delta:float, moving:Vector2):
 	if is_on_floor():
 		var current_speed = velocity.length()
 		var current_dir = velocity/current_speed if current_speed > 0.0 else Vector3.ZERO
-		var slow_by = FRICTION*delta if current_speed > STOP_SPEED else current_speed
+		var edge_scale = (1.0 if $leading_edge.is_colliding() or moving else 3.0)
+		var slow_by = FRICTION*delta*edge_scale if current_speed > STOP_SPEED or moving else current_speed
 		velocity -= current_dir*slow_by
 
 func accelerate(wish_3:Vector3,delta:float):
@@ -70,5 +105,5 @@ func accelerate_air(wish_3:Vector3,delta:float):
 	var vel_h = velocity
 	vel_h.y = 0.0
 	var current_speed = vel_h.dot(wish_3)
-	var add_speed = clamp(delta*ACCEL*0.1, 0.0, speed*0.75-current_speed)
+	var add_speed = clamp(delta*ACCEL*0.15, 0.0, speed*0.75-current_speed)
 	velocity += wish_3 * add_speed
